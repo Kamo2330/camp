235d9feceb1210d345533669922d71e6796cafbc
Kamohelo <kamohelo9274@gmail.com>
2025-09-24 13:16:17 +0200

    Added full project files

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zabalaza.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LookAroundSecurity.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
